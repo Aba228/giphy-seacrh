8356851e8c7632771f3e3ac7294490b740d160d1
mihael326 <96523551+mihael326@users.noreply.github.com>
2022-09-26 12:39:33 +0300

    Update searchGif.ts

--- src/searchGif.ts
+++ src/searchGif.ts
@@ -4,12 +4,8 @@ export async function searchGif(key: string, name: string, searchRandomly: boole
     let response = await axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${encodeURIComponent(key)}&q=${encodeURIComponent(name)}`)
         .then(function (gif) {
             let searched;
-            if(searchRandomly){
-                searched = gif.data.data[Math.floor(Math.random() * gif.data.data.length)].url
-            } 
-            if(!searchRandomly){
-                searched = gif.data.data[0].url
-            }
+            if(searchRandomly) searched = gif.data.data[Math.floor(Math.random() * gif.data.data.length)].url 
+            if(!searchRandomly) searched = gif.data.data[0].url
             return searched
         })
         return response
